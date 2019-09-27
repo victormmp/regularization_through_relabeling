bad87acba4e79cc3b7c5b0511adfef4d25d244c9
victormmp1 <victor.magalhaes@zahpee.com>
2019-09-27 16:17:20 -0300

    Add scripts

---
+++ moons.py
@@ -0,0 +1,321 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 23 19:22:55 2019
+
+@author: victor
+"""
+
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split, cross_validate, ShuffleSplit
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.datasets import make_moons
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import os
+from os.path import join
+
+from utils import euclidian, plot_plain_separator
+
+random_seed = 42
+random.seed(random_seed)
+np.random.seed(random_seed)
+dataset_name="Moons"
+
+#%% Generate classes
+
+print('Generating classes')
+
+samples = 3000
+features = 2
+
+x, y = make_moons(n_samples=samples, random_state=random_seed, noise=0.25)
+
+
+for c in range(2):
+    cl = y == c
+    plt.scatter(x[cl, 0], x[cl, 1],s=20, edgecolor='black')
+    
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=random_seed)
+y_train_backup = y_train
+
+#%% Define model for classification
+
+print('Building model')
+# Do not interrupt the training before end of the epochs, to force an 
+# overfitting
+
+n_epochs=50000
+
+model_1 = {
+        'hidden_layer_sizes': (500,),
+        'activation': 'relu',
+        'alpha': 0,
+        'max_iter': n_epochs,
+        'random_state': random_seed,
+        'tol':1e-4,
+        'verbose': True,
+        'n_iter_no_change': n_epochs
+        }
+
+model_2 = {
+        'hidden_layer_sizes': (500,),
+        'activation': 'relu',
+        'alpha': 0,
+        'shuffle': False,
+        'batch_size': 1,
+        'learning_rate': 'adaptive',
+        'learning_rate_init': 0.05,
+        'max_iter': n_epochs,
+        'random_state': random_seed,
+        'tol':1e-4,
+        'verbose': True,
+        'n_iter_no_change': n_epochs
+        }
+
+model_3 = {
+        'hidden_layer_sizes': (500,),
+        'solver': 'sgd',
+        'activation': 'tanh',
+        'alpha': 0,
+        'learning_rate': 'adaptive',
+        'learning_rate_init': 0.05,
+        'max_iter': n_epochs,
+        'random_state': random_seed,
+        'tol':1e-4,
+        'verbose': True,
+        'n_iter_no_change': n_epochs
+        }
+
+model_4 = {
+        'hidden_layer_sizes': (100,),
+        'activation': 'relu',
+        'alpha': 0,
+        'max_iter': n_epochs,
+        'random_state': random_seed,
+        'tol':1e-12,
+        'verbose': True,
+        'n_iter_no_change': n_epochs
+        }
+
+model = MLPClassifier(**model_4)
+
+#%% Train model
+
+print('Training model without adjustment')
+model.fit(x_train, y_train)
+
+#%% Test
+
+print('Getting score for model without adjustment')
+
+score_test = model.score(x_test, y_test)
+score_train = model.score(x_train, y_train)
+norm_weights_hidden = np.linalg.norm(model.coefs_[0])
+norm_weights_out = np.linalg.norm(model.coefs_[1])
+
+print(f'[RESULT]\nScore Train = {score_train:0.4f}'
+      f'\nScore test = {score_test:0.4f}'
+      f'\nRatio = {score_test/score_train:0.4f}'
+      f'\nNorm Weights Hidden = {norm_weights_hidden}'
+      f'\nNormWeights Out = {norm_weights_out}')
+
+y_pred = model.predict(x_test)
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+#%%
+#ss = ShuffleSplit(n_splits=5, test_size=0.3, random_state=random_seed)
+#cv_results = cross_validate(model, x, y, cv=ss, n_jobs = 6, verbose=2, return_train_score=True)
+#print(cv_results)
+
+#%% Save result tables as Latex format
+
+print('Saving latex tables')
+
+cm = confusion_matrix(y_test, y_pred)
+test_name=f'Treinamento sem Ajuste {dataset_name}'
+table_confusion = r"""
+\begin{{table}}[H]
+\centering
+\caption{{Matrix de confusão com dataset de treinamento para {test_name} }}
+\label{{tab:matrix confusao {test_name} }}
+\begin{{tabular}}{{cc}}
+\hline
+\multicolumn{{2}}{{c}}{{ Matriz de Confusão para {test_name} }} \\ \hline
+{tp} & {fp} \\
+{fn} & {tn} \\ \hline
+\end{{tabular}}
+\end{{table}}
+
+\begin{{table}}[H]
+\centering
+\caption{{Acurácia para {test_name} }}
+\label{{tab:acuracia {test_name} }}
+\begin{{tabular}}{{lr}}
+\hline
+\multicolumn{{2}}{{c}}{{Acurácia}} \\ \hline
+Conjunto de Treinamento & {score_train:0.4f} \\
+Conjunto de teste & {score_test:0.4f} \\
+Razão & {score_ratio:0.4f} \\ \hline
+\multicolumn{{2}}{{c}}{{Norma dos Pesos}} \\ \hline
+Camada escondida & {norm_weights_hidden} \\
+Camada de saída & {norm_weights_out} \\ \hline
+\end{{tabular}}
+\end{{table}}
+
+""".format(
+    test_name=test_name,
+    tp=cm[0,0],
+    fp=cm[0,1],
+    fn=cm[1,0],
+    tn=cm[1,1],
+    score_train=score_train,
+    score_test=score_test,
+    score_ratio=score_test/score_train,
+    norm_weights_hidden=norm_weights_hidden,
+    norm_weights_out=norm_weights_out
+)
+
+with open(join('..', 'Artigo_1_RNA', 'tables', fr'{"_".join(test_name.lower().split())}.txt'), 'w') as file:
+    file.write(table_confusion)
+
+#%% Plot separator
+
+print('Saving plot for model without adjustment')
+
+plt.clf()
+
+for c in range(2):
+    cl = y_train == c
+    plt.scatter(x_train[cl, 0], x_train[cl, 1],s=20, edgecolor='k')
+    
+plot_plain_separator(model, x, save=fr'{"_".join(dataset_name.lower().split())}_nao_ajustada', grid_range=(-3, 3))
+
+#%% Get distance matrix
+
+#distance_matrix = np.zeros((x.shape[0], x.shape[0]))
+#
+#for sample_a in range(x.shape[0]):
+#    
+#    if sample_a > 0 and sample_a % int(0.1 * x.shape[0]) == 0:
+#        print(f'Progress: {sample_a / int(0.1 * x.shape[0]) * 10} % '
+#              f'({sample_a} samples from {x.shape[0]} total samples)')
+#    
+#    for sample_b in range(x.shape[0]):
+#        distance_matrix[sample_a, sample_b] = euclidian(x[sample_a], x[sample_b])
+#
+#classes = list(Counter(y).keys())
+
+#%% Use KNN to find unsure samples
+
+print('Performing ajustment with KNN')
+
+classifier = KNeighborsClassifier(n_neighbors=10)
+
+y_classes = []
+for index in range(x_train.shape[0]):
+    x_t = np.delete(x_train, index, 0)
+    y_t = np.delete(y_train, index, 0)
+    classifier.fit(x_t, y_t)
+    y_classes.append(classifier.predict([x_train[index]])[0])
+
+errors = y_train - y_classes
+
+wrong_classes = np.where(errors != 0)[0]
+
+print('Fixing training samples with adjustment')
+
+for i in wrong_classes:
+    y_train[i] = 0 if y_train[i] == 1 else 1
+
+#%%
+#x_h = np.concatenate([x_train, x_test])
+#y_h = np.concatenate([y_train, y_test])
+#cv_results_2 = cross_validate(model, x_h, y_h, cv=3, n_jobs = 4, verbose=4, return_train_score=True)
+#print(cv_results)
+
+#%% Retrain
+    
+print('Retraining model after adjustment')
+
+model.fit(x_train, y_train)
+
+score_test = model.score(x_test, y_test)
+score_train = model.score(x_train, y_train)
+norm_weights_hidden = np.linalg.norm(model.coefs_[0])
+norm_weights_out = np.linalg.norm(model.coefs_[1])
+
+print(f'[RESULT]\nScore Train = {score_train:0.4f}'
+      f'\nScore test = {score_test:0.4f}'
+      f'\nRatio = {score_test/score_train:0.4f}'
+      f'\nNorm Weights Hidden = {norm_weights_hidden}'
+      f'\nNormWeights Out = {norm_weights_out}')
+
+y_pred = model.predict(x_test)
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+#%% Save result tables as Latex format
+
+print('Saving latex tables for model adjusted')
+
+cm = confusion_matrix(y_test, y_pred)
+test_name=f'Treinamento com Ajuste {dataset_name}'
+table_confusion = r"""
+\begin{{table}}[H]
+\centering
+\caption{{Matrix de confusão com dataset de treinamento para {test_name} }}
+\label{{tab:matrix confusao {test_name} }}
+\begin{{tabular}}{{cc}}
+\hline
+\multicolumn{{2}}{{c}}{{ Matriz de Confusão para {test_name} }} \\ \hline
+{tp} & {fp} \\
+{fn} & {tn} \\ \hline
+\end{{tabular}}
+\end{{table}}
+
+\begin{{table}}[H]
+\centering
+\caption{{Acurácia para {test_name} }}
+\label{{tab:acuracia {test_name} }}
+\begin{{tabular}}{{lr}}
+\hline
+\multicolumn{{2}}{{c}}{{Acurácia}} \\ \hline
+Conjunto de Treinamento & {score_train:0.4f} \\
+Conjunto de teste & {score_test:0.4f} \\
+Razão & {score_ratio:0.4f} \\ \hline
+\multicolumn{{2}}{{c}}{{Norma dos Pesos}} \\ \hline
+Camada escondida & {norm_weights_hidden} \\
+Camada de saída & {norm_weights_out} \\ \hline
+\end{{tabular}}
+\end{{table}}
+
+""".format(
+    test_name=test_name,
+    tp=cm[0,0],
+    fp=cm[0,1],
+    fn=cm[1,0],
+    tn=cm[1,1],
+    score_train=score_train,
+    score_test=score_test,
+    score_ratio=score_test/score_train,
+    norm_weights_hidden=norm_weights_hidden,
+    norm_weights_out=norm_weights_out
+)
+
+with open(join('..', 'Artigo_1_RNA', 'tables', fr'{"_".join(test_name.lower().split())}.txt'), 'w') as file:
+    file.write(table_confusion)
+
+#%% Plot separator
+
+print('Saving figure for model adjusted')
+
+plt.clf()
+for c in range(2):
+    cl = y_train == c
+    plt.scatter(x_train[cl, 0], x_train[cl, 1],s=20, edgecolor='k')
+    
+plot_plain_separator(model, x, save=fr'{"_".join(dataset_name.lower().split())}_ajustada', grid_range=(-3, 3))
+
--- utils.py
+++ utils.py
@@ -7,6 +7,8 @@ Created on Tue Sep 24 20:13:30 2019
 
 import numpy as np
 from matplotlib import pyplot as plt
+import os
+from os.path import join
 
 def euclidian(a, b):
     """
@@ -26,7 +28,12 @@ def euclidian(a, b):
     return np.sqrt(np.sum(np.power(x - y, 2)))
 
 
-def plot_plain_separator(model, x, grid_size=1000, grid_range=(-5, 15), save=None, path=r'..\Artigo_1_RNA\Imagens'):
+def plot_plain_separator(model, 
+                         x, 
+                         grid_size=1000, 
+                         grid_range=(-5, 15),
+                         save=None, 
+                         path=join('..', 'Artigo_1_RNA', 'Imagens')):
     x_lab = np.linspace(grid_range[0], grid_range[1], num=grid_size)
     y_lab = np.linspace(grid_range[0], grid_range[1], num=grid_size)
     x1, x2 = np.meshgrid(x_lab, y_lab)
@@ -35,7 +42,8 @@ def plot_plain_separator(model, x, grid_size=1000, grid_range=(-5, 15), save=Non
     z = model.predict(x_grid)
     
     z = z.reshape([1000,1000])
-    plt.contour(x1, x2, z, levels=[0], colors=('black',), linewidths=(2,))
+    plt.contour(x1, x2, z, levels=[0], colors=('cyan',), linewidths=(2.5,))
+#    plt.contour(x1, x2, z, linewidths=(2,))
     if save:
-        plt.savefig(fr'{path}\{save}.png')
+        plt.savefig(join(fr'{path}', fr'{save}.png'))
         
